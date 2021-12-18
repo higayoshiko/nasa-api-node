3122e8f13cdd44117cb578f53d07489f2466a756
higayoshiko <higayoshiko@yahoo.com>
2021-12-18 15:23:57 +0900

    hide api keys

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
+const bodyParser = require("body-parser");
 const https = require("https");
+const port = 3000;
+//require DontEnv for environment variables from .env file
+require("dotenv").config();
 
 //set the view engine to ejs
 app.set("view engine", "ejs");
@@ -15,34 +18,52 @@ app.use(express.static("public"));
 app.use("/css", express.static(__dirname + "public/css"));
 app.use("/img", express.static(__dirname + "public/img"));
 
+//define root route
 app.get("/", function(req, res) {
+  //render and send
   res.render("index");
 });
 
-
+//define weather route
 app.get("/weather", function(req, res) {
-
-  const url = "https://api.nasa.gov/insight_weather/?api_key=PULvfd8KgJN1IErBCYzGEPqDScihqeRxZS8Zvy0V&feedtype=json&ver=1.0";
+  //use hidden api key
+  const keys = process.env.API_KEY;
+  const url = `https://api.nasa.gov/insight_weather/?api_key=${keys}`;
 
   https.get(url, function(response) {
     let result = "";
+      //run when getting the data
     response.on("data", function(data) {
+      //add result one by one to empty variable
       result += data;
     });
-    response.on("end", function() {
-      const parsed = JSON.parse(result);
-      const solKeys = parsed.sol_keys.join("");
-      const highTemp = parsed[solKeys]["AT"]["mx"];
-      const lowTemp = parsed[solKeys]["AT"]["mn"];
 
-        res.render("weather", {hello: highTemp});
+    //run at end of response
+    response.on("end", function() {
+      //parse result to JSON
+      const parsed = JSON.parse(result);
+
+      //get current SOL
+      const solKeys = parsed.sol_keys[0];
+      //get high & low temp and round them off
+      const highTemp = Math.round(parsed[solKeys]["AT"]["mx"]);
+      const lowTemp = Math.round(parsed[solKeys]["AT"]["mn"]);
+      //get average pressure and round it off
+      const pressure = Math.round(parsed[solKeys]["PRE"]["av"]);
+      //get current season
+      const season = parsed[solKeys]["Season"];
+      //get average wind speed
+      const wind = Math.round(parsed[solKeys]["HWS"]["av"]);
+        //render and send
+        res.render("weather",
+          {solKeys: solKeys, highTemp: highTemp,
+          lowTemp: lowTemp, pressure: pressure,
+          season: season, wind: wind});
     });
   });
-
-
 });
 
-
-app.listen(3000, function() {
+//listen to port 3000;
+app.listen(port, function() {
   console.log("server started");
 });
